3c5bf5364e0fee8212504ab51d561dc5ce21cb1a
melvyn <melvyn.rouault@gmail.com>
2021-03-25 19:26:28 +0100

    controller set up

--- src/pokemon/pokemon.controller.ts
+++ src/pokemon/pokemon.controller.ts
@@ -1,4 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { PokemonService } from './pokemon.service';
 
 @Controller('pokemon')
-export class PokemonController {}
+export class PokemonController {
+
+
+  
+}
